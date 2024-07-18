7c856b9900b49ca06e26bb2128f6890eaff8782f
carlitos040506 <carloscarvajaldiaz86@gmail.com>
2024-07-18 17:33:22 -0500

    Actualización

---
+++ OneDrive/Documentos/JS Carlos/Entorno/LineDoctor/asistencia/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('paginas/nosotros/', views.nosotros, name='nosotros'),
+    path('estudiantes/crear/', views.crear, name='crear'),
+    path('estudiantes/editar/', views.editar, name='editar'),
+    path('estudiantes/index/', views.index, name='index'),
+]
---
+++ OneDrive/Documentos/JS Carlos/Entorno/LineDoctor/asistencia/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Bienvenidos a LineDoctor Asistencia")
+    
+# Create your views here.
+
+def nosotros(request):
+    return render(request, 'paginas/nosotros.html')
+
+
+def index(request):
+    return render(request, 'estudiantes/index.html')
+
+def crear(request):
+    return render(request, 'estudiantes/crear.html')
+
+def editar(request):
+    return render(request, 'estudiantes/editar.html')
